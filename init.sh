a5bf54aa905e8840dba43751070e88c874e12466
Evelina <82104690+zowiter@users.noreply.github.com>
2023-03-01 22:34:01 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
-sudo /etc/init.d/mysql start
+gunicorn --bind='0.0.0.0:8080' hello:app
