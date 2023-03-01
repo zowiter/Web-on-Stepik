781ad930de1d87eb40ccc0e1f9788db4e2637484
Evelina <82104690+zowiter@users.noreply.github.com>
2023-03-01 22:43:31 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-gunicorn -b 0.0.0.0:8080 -c ~/web/hello.py hello:application 
+gunicorn -b 0.0.0.0:8080 -c ~/web/hello.py hello:app
